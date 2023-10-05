da6bc733879057af6939423114b37accc6bdcf3e
Adam Myers <myers.adam.k@gmail.com>
2023-10-05 11:59:15 -0400

    Initial commit from start.spring.io

---
+++ src/main/kotlin/exercise/sudoku/SudokuApplication.kt
@@ -0,0 +1,11 @@
+package exercise.sudoku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SudokuApplication
+
+fun main(args: Array<String>) {
+	runApplication<SudokuApplication>(*args)
+}
---
+++ src/test/kotlin/exercise/sudoku/SudokuApplicationTests.kt
@@ -0,0 +1,13 @@
+package exercise.sudoku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
